d84d947739a48f9f7c57f6e654d573ce42203509
frank-Regal <fregal@utexas.edu>
2024-03-28 21:27:52 -0500

    modifications

--- setup.py
+++ setup.py
@@ -14,12 +14,6 @@ setup(
     description="",
     author="OpenAI",
     packages=find_packages(exclude=["tests*"]),
-    install_requires=[
-        str(r)
-        for r in pkg_resources.parse_requirements(
-            open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
-        )
-    ],
     include_package_data=True,
     extras_require={'dev': ['pytest']},
 )
